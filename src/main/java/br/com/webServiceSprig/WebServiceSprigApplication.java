b1ef8f561075a0f6a666cf624e093051e66e86bd
Renan Monteiro Batista <55768865+renanmonteiroo@users.noreply.github.com>
2022-05-17 10:36:10 -0300

    Add files via upload
    Criação do projeto

---
+++ src/main/java/br/com/webServiceSprig/WebServiceSprigApplication.java
@@ -0,0 +1,13 @@
+package br.com.webServiceSprig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceSprigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceSprigApplication.class, args);
+	}
+
+}
